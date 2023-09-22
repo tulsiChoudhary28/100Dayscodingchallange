7c21b20503940109844301bede400b73a74dfc70
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-22 15:27:58 +0530

    Add files via upload
    Conditional Statement :  If Else condition for Even Numbers Or Odd Numbers

---
+++ ConditionalStatement.java
@@ -0,0 +1,16 @@
+class ConditionalStatement
+{
+  public static void main(String[]args)
+{
+          int a = 15;
+  
+
+   if(a%2==0)
+      
+             System.out.println(a+" is even number");
+
+  else 
+   
+            System.out.println(a+" is odd number");
+   }
+}

c928ddac3e48dc844cff2af88adfbe3263d8703c
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-22 15:31:33 +0530

    Add files via upload
    Swapping Of Two Numbers Code Of Java Programming

---
+++ Swaping Numbers.java
@@ -0,0 +1,18 @@
+class SwappingNumbers
+{ 
+  public static void main(String[]args)
+{
+     int x = 100;
+     int y = 200;
+     int z;
+     
+
+     z = x;
+     x = y;
+     y = x;
+ 
+
+ System.out.println("x = "+x);
+ System.out.println("y = "+y);
+}
+}

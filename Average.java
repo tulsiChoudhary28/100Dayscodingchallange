ba5f24d601466424e0c95d4ad66379e3f7b8acbb
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-22 15:24:18 +0530

    Add files via upload
    Average . java  # program to Average of 5 numbers in code

---
+++ Average.java
@@ -0,0 +1,13 @@
+class Average
+{
+ public static void main(String[]args)
+{
+    int num1 =5;
+    int num2 =10;
+    int num3 =15;
+    int num4 =20;
+    int num5 =25;
+
+ System.out.println("Average of five numbers is :"+ num1+num2+num3+num4+num5/5);
+}
+}

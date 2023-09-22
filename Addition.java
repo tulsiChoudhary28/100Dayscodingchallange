af6be33344b608c48484aa0655b2fd10b0a4446c
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-22 15:20:38 +0530

    Add files via upload
    Addition . java  contains code is Addition of 5 numbers

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition
+{
+  public static void main(String[]args)
+{
+     int a=10;
+     int b=20;
+     int c=30;
+     int d=40;
+     int e=45;
+  
+  System.out.println("Addition ="+(a+b+c+d+e));
+}
+}
+

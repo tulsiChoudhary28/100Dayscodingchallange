20fa53f3d478f24afb23409ff36885604465bb1c
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-22 15:55:35 +0530

    Add files via upload
    Program to check is Divisibility in the numbers is divisible by 3 & 7 in java code of contains

---
+++ DivisibilityNumbers.java
@@ -0,0 +1,16 @@
+class DivisibilityNumbers
+{
+  public static void main(String[]args)
+{
+ 
+     int x = 10;
+  
+  if(x%3==0 && x%7==0)
+
+            System.out.println("yes");
+
+  else
+  
+           System.out.println("no");
+  }
+}

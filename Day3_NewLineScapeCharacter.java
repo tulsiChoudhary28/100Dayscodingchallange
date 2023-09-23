6a02bb66eab61315615ac25eca25eee691ebe9dd
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-23 16:22:20 +0530

    Add files via upload
    New line with using \n  to print escape characters is contains of code in java

---
+++ Day3_NewLineScapeCharacter.java
@@ -0,0 +1,7 @@
+class NewLineScapeCharacter
+{
+    public static void main(String[]args)
+{
+    System.out.println("Easy hai code \n Easy hai code\n Easy hai code \n Easy hai code \n Easy hai code \n");
+  }
+}

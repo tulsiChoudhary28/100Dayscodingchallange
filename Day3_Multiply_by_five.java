a15d2d6e426cfdcfbcbaadb96d14fe52cbe68e24
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-23 16:23:56 +0530

    Add files via upload
    Multiplication by five in the numbers is contains of code in java

---
+++ Day3_Multiply_by_five.java
@@ -0,0 +1,12 @@
+class Multiply_by_five
+{
+  public static void main(String[]args)
+{
+   int a = 20;
+ 
+ if(a%5==0)
+            System.out.println(a+": Yes");
+     else
+            System.out.println(a+": No");
+  }
+}

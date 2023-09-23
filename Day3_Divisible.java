32d753fa8efdc1dc450e328d8bab1eb183f1f4c9
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-23 16:28:01 +0530

    Add files via upload
    Divisibility in the numbers is contains of code in java

---
+++ Day3_Divisible.java
@@ -0,0 +1,13 @@
+class Divisible
+{
+   public static void main (String[]args)
+ {
+    int a = 15;
+       
+    if (a%3==0 || a%7==0)
+ 
+         System.out.println(a+": is divisible number");
+  else
+         System.out.println(a+": is not divisible number");
+  }
+}

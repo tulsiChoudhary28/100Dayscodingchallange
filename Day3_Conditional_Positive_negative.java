6c1a531c0120a51664541a66f8e0f72023e46da8
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-23 16:19:45 +0530

    Add files via upload
    Conditional statement if else with number is positive or number is negative contains of code

---
+++ Day3_Conditional_Positive_negative.java
@@ -0,0 +1,11 @@
+class Conditional_Positive_negative
+{
+  public static void main(String[]args)
+ {    
+        int x = 4;
+if(x>=0)
+  System.out.println(x+": is positive");
+else
+  System.out.println(x+": is negative");
+  }
+} 

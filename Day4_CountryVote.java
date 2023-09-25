2630e968be61bb79b400c1288fee5dc35785a188
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-25 16:46:19 +0530

    Add files via upload

---
+++ Day4_CountryVote.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class CountryVote
+
+{
+  public static void main(String[]args)
+{
+  System.out.println("Enter age");
+Scanner sc=new Scanner(System.in);
+int age=sc.nextInt();
+
+     if(age>=18)
+ 
+                 System.out.println("yes");
+       else
+                 System.out.println("no");
+   }
+}   

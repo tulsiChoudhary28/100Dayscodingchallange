4412dd1aab4268927bae5bea002e16b79d191df1
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-25 16:47:13 +0530

    Add files via upload

---
+++ Day_4Cheak_Number.java
@@ -0,0 +1,12 @@
+class CheakNumber
+{
+ public static void main(String[] args)
+{
+            int a = 44;
+if(a>21&& a<=575)
+
+              System.out.println(" yes number is valid");
+else
+             System.out.println(" number is not valid");
+}
+}

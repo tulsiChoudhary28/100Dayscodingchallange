45067fdd7bf71c26313e30826273e8561e7d20f3
tulsiChoudhary28 <145674474+tulsiChoudhary28@users.noreply.github.com>
2023-09-25 16:47:47 +0530

    Add files via upload

---
+++ Day_4Alphabate.java
@@ -0,0 +1,12 @@
+class Alphabate
+{
+  public static void main(String[]args)
+{
+    char value ='&';
+
+  if(value>='a'&& value<='z'|| value>='A'&& value<='Z')
+         System.out.println("Alphabate");
+ else
+         System.out.println("Not Alphabate");
+}
+}
